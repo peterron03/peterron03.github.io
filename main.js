0ab3ca7b34efbfc371333dd79bd636416d5e7f2d
Peter <77168867+peterron03@users.noreply.github.com>
2025-08-06 02:39:39 -0500

    refreshButton

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const combinedVisits = document.getElementById('combinedPlaceVisits');
 const combinedFavorites = document.getElementById('combinedPlaceFavorites');
 const combinedPlayers = document.getElementById('combinedPlayerCount');
+const refreshButton = document.getElementById('refreshButton');
 
 const gameLabels = {
   "6691764291": {
@@ -88,28 +89,6 @@ async function updatePlayerCountNew() {
   combinedFavorites.textContent = `${addCommas(Number(combinedStats.favoritedCount))} favorites`;
   combinedPlayers.textContent = `${addCommas(Number(combinedStats.playing))} players`;
 }
-
-/* async function updatePlayerCount() {
-  gameNameText.textContent = "Loading...";
-  playerCountText.textContent = "Loading...";
-  visitsText.textContent = "Loading...";
-  favoritesText.textContent = "Loading...";
-  try {
-    const response = await fetch(`https://games.roproxy.com/v1/games?universeIds=6691764291`);
-    const data = await response.json();
-    const gameName = data.data[0].name;
-    const visits = data.data[0].visits;
-    const favorites = data.data[0].favoritedCount;
-    const playerCount = data.data[0].playing;
-    gameNameText.textContent = gameName;
-    playerCountText.textContent = `${playerCount} players`;
-    visitsText.textContent = `${visits} visits`;
-    favoritesText.textContent = `${favorites} favorites`;
-  } catch (error) {
-    playerCountText.textContent = `Error: ${error.message}`;
-    console.error(error);
-  }
-} */
 
 refreshButton.addEventListener('click', updatePlayerCountNew);
 
