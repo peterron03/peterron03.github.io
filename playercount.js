803bc0befc68f26527a64311b0f8a6621ee8cc5a
Peter <77168867+peterron03@users.noreply.github.com>
2025-08-06 01:16:46 -0500

    Update playercount.js

--- playercount.js
+++ playercount.js
@@ -1,3 +1,6 @@
+const gameNameText = document.getElementById('gameName');
+const visitsText = document.getElementById('placeVisits');
+const favoritesText = document.getElementById('placeFavorites');
 const playerCountText = document.getElementById('playerCount');
 const refreshButton = document.getElementById('refreshButton');
 
@@ -6,8 +9,14 @@ async function updatePlayerCount() {
   try {
     const response = await fetch(`https://games.roproxy.com/v1/games?universeIds=6691764291`);
     const data = await response.json();
+    const gameName = data.data[0].name;
+    const visits = data.data[0].visits;
+    const favorites = data.data[0].favoritedCount;
     const count = data.data[0].playing;
-    playerCountText.textContent = `Current players: ${count}`;
+    gameNameText.textContent = gameName;
+    playerCountText.textContent = `${count} players`;
+    visitsText.textContent = `${count} visits`;
+    favoritesText.textContent = `${count} favorites`;
   } catch (error) {
     playerCountText.textContent = `Error: ${error.message}`;
     console.error(error);
