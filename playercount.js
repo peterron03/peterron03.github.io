87246a9a4257bf62e2eca4b0df3a80d8306a3d90
Peter <77168867+peterron03@users.noreply.github.com>
2025-08-06 01:20:46 -0500

    Update playercount.js

--- playercount.js
+++ playercount.js
@@ -5,7 +5,10 @@ const playerCountText = document.getElementById('playerCount');
 const refreshButton = document.getElementById('refreshButton');
 
 async function updatePlayerCount() {
+  gameNameText.textContent = "Loading...";
   playerCountText.textContent = "Loading...";
+  visitsText.textContent = "Loading...";
+  favoritesText.textContent = "Loading...";
   try {
     const response = await fetch(`https://games.roproxy.com/v1/games?universeIds=6691764291`);
     const data = await response.json();
