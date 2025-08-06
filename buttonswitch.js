56209037e6791dc123e3a65d30e90259552577f7
Peter <77168867+peterron03@users.noreply.github.com>
2025-08-06 07:37:56 -0500

    color changing button

--- buttonswitch.js
+++ buttonswitch.js
@@ -1,11 +1,16 @@
 const button = document.getElementById('switchButton');
 const paragraph = document.getElementById('buttonText');
 
-// Initialize display
 let status = Number(localStorage.getItem("buttonClicks1")) || 0;
 paragraph.textContent = `The Button has been clicked ${status} times`;
 
 button.addEventListener('click', () => {
+  const r = Math.round(Math.random() * 255)
+  const g = Math.round(Math.random() * 255)
+  const b = Math.round(Math.random() * 255)
+
+  button.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  
   status += 1;
   localStorage.setItem("buttonClicks1", status);
   paragraph.textContent = `The Button has been clicked ${status} times`;
